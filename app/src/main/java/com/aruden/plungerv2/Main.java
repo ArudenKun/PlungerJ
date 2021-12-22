c9bb86e2282bb53c0df59262fabc02c4a91117a0
Aruden <84431560+ArudenKun@users.noreply.github.com>
2021-12-22 17:23:52 +0800

    Added Logging

--- app/src/main/java/com/aruden/plungerv2/Main.java
+++ app/src/main/java/com/aruden/plungerv2/Main.java
@@ -1,7 +1,26 @@
 package com.aruden.plungerv2;
 
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
 public class Main {
-    public static void main(String[] args) {
-        
+
+    private Main() throws LoginException {
+
+        String token = "ODg5NTE1MDU1NjE0NDAyNjgx.YUiXcw.39YU7zFzF4pIRRjsiILSokfMKl8";
+
+        JDABuilder.createDefault(token,
+            GatewayIntent.GUILD_MEMBERS,
+            GatewayIntent.GUILD_MESSAGES
+        )
+        .addEventListeners(new Listener())
+        .setActivity(Activity.watching("Plunger"))
+        .build();
+    }
+    public static void main(String[] args) throws LoginException {
+        new Main();
     }
 }
